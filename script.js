4c8826a946bcf4a98a83aad687cc83a401ee1839
alessio-civitella <alessio.civitella19@gmail.com>
2024-11-18 16:58:52 +0100

    Feat: pagina completata

---
+++ script.js
@@ -0,0 +1,12 @@
+const emailList = document.getElementById("email-list");
+
+for (let i = 1; i <= 10; i++) {
+    
+    axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
+        const emailCur = resp.data.response;
+        emailList.innerHTML += `<li>${emailCur}</li>`
+        console.log(emailCur);
+        
+    });  
+}
+
